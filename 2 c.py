6080f5595de726e3587a7a9d282295f88597a25b
vellinDam <118042632+vellinDam@users.noreply.github.com>
2022-12-08 23:10:45 +0700

    Add files via upload

---
+++ 2 c.py
@@ -0,0 +1,11 @@
+kalimat = input("masukkan kata atau angka: ")
+def reserve(kalimat):
+    str= ""
+    for i in kalimat:
+        str = i + str
+    return str
+print(reserve(kalimat))
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
